c3552242fff368564e05a609ec39ea9d12f2d447
xonin-hush <xonindev@gmail.com>
2024-06-23 07:45:38 +0100

    Figure out how to deal with buckets

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Odin-Hash-Map
+learned that you can use class's own method in other method by this.method()
--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+}
 class Hashmap {
+  constructor() {
+    this.buckets = [];
+    this.bucketSize = [];
+    // this.loadFactor=0.75
+    // this.product=this.bucketSize*this.loadFactor
+  }
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
-      console.log(hashCode)
+
       hashCode = hashCode % 16;
     }
     console.log(hashCode);
     return hashCode;
   }
+  set(key, value) {
+    let hashCode = this.hash(key);
+    let node = new Node(key.value);
+    this.buckets[hashCode] = node;
+    console.log(this.buckets)
+    // console.log(hashCode);
+    // let index = hash(key);
+    // console.log(index);
+    // if (index < 0 || index >= bucket.length) {
+    // throw new Error("Trying to access index out of bound");
+    // }
+  }
 }
 L = new Hashmap();
-// L.hash("hello");
-// L.hash("xonin");
-L.hash("leda");
-// L.hash("some good guy");
+L.hash("hello");
+L.hash("edla");
+L.set("edla", "value");
+// L.hash("leda");
+// L.hash("lead")
+// L.set("hi", "value");
