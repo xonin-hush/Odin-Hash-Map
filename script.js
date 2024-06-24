4a7200c76dc00a6c1e6104199d74bb0eed9cada1
xonin-hush <xonindev@gmail.com>
2024-06-24 21:25:29 +0100

    Fix set(key,value) method

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+    this.next = null;
+  }
+}
 class Node {
   constructor(key, value) {
     this.key = key;
@@ -8,8 +14,8 @@ class Hashmap {
   constructor() {
     this.buckets = [];
     this.buckets.length = 16;
-    // this.loadFactor=0.75
-    // this.product=this.bucketSize*this.loadFactor
+    this.loadFactor = 0.75;
+    this.product = this.bucketSize * this.loadFactor;
   }
 
   hash(key) {
@@ -20,37 +26,57 @@ class Hashmap {
 
       hashCode = hashCode % 16;
     }
-    console.log(hashCode);
     return hashCode;
   }
 
   set(key, value) {
-    let something = 4;
     let index = this.hash(key);
+
     if (index < 0 || index >= this.buckets.length) {
       throw new Error("Trying to access index out of bound");
     } else {
       let node = new Node(key, value);
+
       if (this.buckets[index] == undefined) {
-        this.buckets[index] = node;
-      }
-      else{
-        if(this.buckets[index].value==node.value){
-          return
-        }
-        else{
-          this.buckets[index]=node;
+        this.buckets[index] = new LinkedList(node);
+        console.log(this.buckets[index].head.key)
+        console.log(0)
+      } else {
+        if (
+          this.buckets[index].head.value == node.value &&
+          this.buckets[index].head.key == node.key
+        ) {
+          console.log(1)
+          return;
+        } else {
+          if (
+            this.buckets[index].head.key == node.key &&
+            this.buckets[index].head.value != node.value
+          ) {
+            this.buckets[index].head = node;
+            console.log(2)
+          }
+
+          if (this.buckets[index].head.key != node.key) {
+            this.buckets[index].head.next = node;
+            // console.log(this.buckets[1].value)
+            // console.log(this.buckets[index].key,node.key)
+            console.log(3)
+            console.log("done")
+          }
         }
       }
     }
+    console.log(this.buckets)
   }
 }
 L = new Hashmap();
-
-L.set("edla", "value");
-L.set("edla", "new");
-L.set("edla", "new");
-
 // L.hash("leda");
 // L.hash("lead")
-// L.set("hi", "value");
+L.set("hi", "value");
+L.set("hi", "value");
+L.set("ih", "value");
+
+L.set("hello","bas")
+L.set("hi","not same value")
+
